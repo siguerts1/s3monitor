666b213e4a6d97c35eb62f6aa6d4de69f52673bc
siguerts1 <samy.iguertsira@gmail.com>
2025-03-07 12:19:14 -0500

    add versioning system

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from helpers.process_bucket_info import process_bucket_info
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+VERSION = "0.1.0"
+
 SIZE_UNITS = {
     "bytes": 1,
     "kb": 1024,
@@ -22,9 +24,14 @@ STORAGE_CLASSES = ["STANDARD", "STANDARD_IA", "ONEZONE_IA", "GLACIER", "DEEP_ARC
 @click.option('--region', default=None, help='Filter buckets by AWS region.')
 @click.option('--size-unit', default='mb', type=click.Choice(SIZE_UNITS.keys(), case_sensitive=False), help='Choose size unit: bytes, kB, MB, GB, TB (default: MB).')
 @click.option('--storage-class', default=None, type=click.Choice(STORAGE_CLASSES, case_sensitive=False), help='Filter objects by storage class (e.g., STANDARD, STANDARD_IA, GLACIER).')
-def main(bucketname, region, size_unit, storage_class):
+@click.option('--version', is_flag=True, help='Show the version and exit.')
+def main(bucketname, region, size_unit, storage_class, version):
     """Entrypoint for the S3 monitoring tool."""
     s3_client = S3Collector("").s3_client
+
+    if version:
+        print(f"S3 Monitor - Version {VERSION}")
+        return
     
     if not bucketname:
         logger.info("No bucket name provided. Retrieving all available buckets.")
